279ca84d6e1375ecef7f2fcad73e0601ff9fbdb6
neangsamoun01 <80339215+neangsamoun01@users.noreply.github.com>
2021-04-07 20:32:34 +0700

    Update Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,23 +5,23 @@ import Rating from './Rating';
 export default function Product(props) {
   const { product } = props;
   return (
-    <div key={this.product._id} className="card">
-      <Link to={`/product/${this.product._id}`}>
-        <img className="medium" src={this.product.image} alt={this.product.name} />
+    <div key={product._id} className="card">
+      <Link to={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt={this.product.name} />
       </Link>
       <div className="card-body">
         <Link to={`/product/${this.product._id}`}>
           <h2>{this.product.name}</h2>
         </Link>
         <Rating
-          rating={this.product.rating}
-          numReviews={this.product.numReviews}
+          rating={product.rating}
+          numReviews={product.numReviews}
         ></Rating>
         <div className="row">
-          <div className="price">${this.product.price}</div>
+          <div className="price">${product.price}</div>
           <div>
-            <Link to={`/seller/${this.product.seller._id}`}>
-              {this.product.seller.seller.name}
+            <Link to={`/seller/${product.seller._id}`}>
+              {product.seller.seller.name}
             </Link>
           </div>
         </div>
